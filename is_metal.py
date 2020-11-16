dcbbae35fab45f71ba8192e59599a0edd97d4b29
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-11-16 13:27:44 +0800

    add roc test
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- is_metal.py
+++ is_metal.py
@@ -1,8 +1,21 @@
+############# (down)load the data ###############
 from matminer.datasets import load_dataset
-# df = load_dataset("matbench_expt_is_metal") # 4k
-df = load_dataset("matbench_mp_is_metal") # 100k
-print(df)
+df = load_dataset("matbench_expt_is_metal") # 4k, ROC-AUC 0.92
+# df = load_dataset("matbench_mp_is_metal") # 100k, 7.2% mem on inode01, ROC-AUC 0.977
+print(df.values.shape)
 
+############# test ROC_AUC value ###########
+import numpy as np
+np.random.seed(18012019)
+from sklearn.metrics import roc_auc_score
+is_metal = df.values[:,1:2]
+is_metal = is_metal.flatten()
+rand_array = np.random.rand(is_metal.shape[0])
+ret1 = roc_auc_score(is_metal.astype(int), rand_array.astype(float))
+ret2 = roc_auc_score(is_metal.astype(int), is_metal.astype(float))
+print(ret1, ret2)
+
+############ k-fold ################# 
 from sklearn.model_selection import KFold
 kf = KFold(n_splits=5, shuffle=True, random_state=18012019)
 print(kf)
