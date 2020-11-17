b852e77f363e1047d1411adaf5d7408a6110c734
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-11-17 22:32:09 +0800

    save in db
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- is_metal.py
+++ is_metal.py
@@ -26,8 +26,8 @@ def cvt_pymatgen2ase(strus):
     ret = []
     for i in range(strus.shape[0]):
         atoms = pymatgen_io_ase.AseAtomsAdaptor.get_atoms(strus[i][0])
-        ret.append([atoms])
-    return np.array(ret)
+        ret.append(atoms)
+    return ret
 
 def check_element_count(X):
     d = {}
@@ -53,58 +53,12 @@ def check_element_count(X):
     print(len(d), d)
     print(len(d_uniq), sorted(d_uniq.items(), key=lambda d: d[1], reverse=True))
 
-def check_clst_count(X, ele_lst, min_cnt, max_cnt, max_length):
-    # find and sort all keys
-    print(X.shape)
-    total_clst_dict = {}
-    # for i in range(X.shape[0]):
-    for i in range(10):
-        if i%10 == 0:
-            print(time.asctime( time.localtime(time.time()) ), i)
-        ele_idx_lst = []
-        for j in range(len(X[i][0])):
-            if X[i][0][j].symbol in ele_lst:
-                ele_idx_lst.append(j)
-        for at_cnts in range(min_cnt, max_cnt):
-            for idx_tup in list(combinations(ele_idx_lst, at_cnts)):
-                clst_pair_lst = [] # value of a cluster
-                for idx_pair in list(combinations(list(idx_tup), 2)):
-                    d = X[i][0].get_distance(idx_pair[0], idx_pair[1], mic=True)
-                    if d > max_length:
-                        break
-                    else:
-                        clst_pair_lst.append((''.join(
-                            sorted([X[i][0][idx_pair[0]].symbol, X[i][0][idx_pair[1]].symbol])
-                            ), d))
-                else:
-                    clst_hash = ''.join(sorted(X[i][0][k].symbol for k in list(idx_tup))) # key of a cluster
-                    if clst_hash in total_clst_dict:
-                        total_clst_dict[clst_hash].append(clst_pair_lst)
-                    else:
-                        total_clst_dict[clst_hash] = [clst_pair_lst]
-    for (k, v) in sorted(total_clst_dict.items(), key=lambda d: len(d[1]), reverse=True):
-        print(k, len(v))
-    # all atoms find the value of keys
-    pass
-
-def cal_BoCs(X):
-    '''
-    input is a np.array of ase atoms, output is np.array of BoCs
-    '''
-    # check_element_count(X)
-    ele_candidates = ['O', 'P', 'S', 'F', 'H', 'Si', 'C', 'B', 'N', 'Cl', 'I', 'Br']
-    min_cnt = 1
-    max_cnt = 3
-    max_clst_size = 5
-    check_clst_count(X, ele_candidates, min_cnt, max_cnt, max_clst_size)
-    # 1. generate whole data
-    # 2. clustering
-    # 3. calculate BoC of each one
-    return X
-
 def extract_features(X):
+    ele_lst = ['O', 'P', 'S', 'F', 'H', 'Si', 'C', 'B', 'N', 'Cl', 'I', 'Br']
     X = cvt_pymatgen2ase(X)
-    return cal_BoCs(X)
+    # delete metal elements
+    # insert to a db
+    return X
 
 model = None
 
